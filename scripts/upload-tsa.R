e6f39f67482d78358e70619aa862450262f721df
alejandrohagan <alejandro.hagan@outlook.com>
2025-05-04 15:47:25 -0500

    print statements in upload tsa script

--- scripts/upload-tsa.R
+++ scripts/upload-tsa.R
@@ -1,6 +1,10 @@
 
+print("Starting TSA upload...")
+print(Sys.getenv("MOTHERDUCK_TOKEN"))  # Make sure this isn't empty
+print("Loaded token, now downloading data...")
+
 tsa_tbl <- awt::download_tsa_daily_volumes()
 
-con <-awt::connect_to_motherduck()
+con <-awt::connect_to_motherduck(motherduck_token = "MOTHERDUCK_TOKEN")
 
 awt::upload_tibble_to_motherduck(tsa_tbl,con,"tsa","main","tsa_passenger_volumes")
