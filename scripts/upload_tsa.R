47cff18a2b5cf139e4b2c796f6485bbf28fd6145
alejandrohagan <alejandro.hagan@outlook.com>
2025-05-10 18:22:26 -0500

    add library upload_tsa.R

--- scripts/upload_tsa.R
+++ scripts/upload_tsa.R
@@ -1,6 +1,7 @@
 
-tsa_tbl <- awt::download_tsa_daily_volumes()
+library(awt)
+tsa_tbl <- download_tsa_daily_volumes()
 
-con <-awt::connect_to_motherduck(motherduck_token = "MOTHERDUCK_TOKEN")
+con <-connect_to_motherduck(motherduck_token = "MOTHERDUCK_TOKEN")
 
-awt::upload_tibble_to_motherduck(tsa_tbl,con,"tsa","main","tsa_passenger_volumes")
+upload_tibble_to_motherduck(tsa_tbl,con,"tsa","main","tsa_passenger_volumes")
