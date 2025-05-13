32e63ef6c411ff384f943fc6e92cf49160272ca4
alejandrohagan <alejandro.hagan@outlook.com>
2025-05-13 18:31:44 -0500

    update script

--- scripts/upload_tsa.R
+++ scripts/upload_tsa.R
@@ -2,12 +2,10 @@
 library(awt)
 
 
-token <- Sys.getenv("MOTHERDUCK_TOKEN")
-stopifnot(nzchar(token))
-message("✅ MotherDuck token loaded (length: ", nchar(token), ")")
-
 tsa_tbl <- download_tsa_daily_volumes()
 
-con <-connect_to_motherduck(motherduck_token = Sys.getenv("MOTHERDUCK_TOKEN"))
+Sys.sleep(10)
+
+con <- connect_to_motherduck(motherduck_token = Sys.getenv("MOTHERDUCK_TOKEN"))
 
 upload_tibble_to_motherduck(tsa_tbl,con,"tsa","main","tsa_passenger_volumes")
