2e959043a099695ee9107f9d23491cafc0150562
alejandrohagan <alejandro.hagan@outlook.com>
2025-05-15 19:48:20 -0500

    added messages

--- scripts/upload_tsa.R
+++ scripts/upload_tsa.R
@@ -2,10 +2,22 @@
 library(awt)
 
 
+message("start script")
+
 tsa_tbl <- download_tsa_daily_volumes()
+
+message("successful download tsas volumes")
+
 
 Sys.sleep(10)
 
+
+message("create mother duck connection")
+
 con <- connect_to_motherduck(motherduck_token = Sys.getenv("MOTHERDUCK_TOKEN"))
+
+Sys.sleep(10)
+
+message("upload data")
 
 upload_tibble_to_motherduck(tsa_tbl,con,"tsa","main","tsa_passenger_volumes")
